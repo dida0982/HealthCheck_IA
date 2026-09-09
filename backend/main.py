7eb703a3bf45df4a9821e1fd24bee7f8cd10dc11
Guilherme Barros JR <131307334+dida0982@users.noreply.github.com>
2026-09-09 16:23:46 -0300

    feat: adicionar estrutura inicial do backend com FastAPI e lógica de captura de resultados na extensão

---
+++ backend/main.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title="HealthCheck IA API",
+    version="1.0.0"
+)
+
+
+# =========================
+# CORS
+# =========================
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# =========================
+# MODELOS DE DADOS
+# =========================
+
+class AnaliseRequest(BaseModel):
+    titulo: str
+    descricao: str
+
+
+class AnaliseResponse(BaseModel):
+    classificacao: str
+    confianca: float
+
+
+# =========================
+# ROTAS
+# =========================
+
+@app.get("/")
+def raiz():
+    return {
+        "mensagem": "HealthCheck IA API funcionando"
+    }
+
+
+@app.post(
+    "/analisar",
+    response_model=AnaliseResponse
+)
+def analisar(dados: AnaliseRequest):
+
+    print("Título recebido:", dados.titulo)
+    print("Descrição recebida:", dados.descricao)
+
+    # RESPOSTA SIMULADA
+    return AnaliseResponse(
+        classificacao="enganosa",
+        confianca=0.82
+    )
